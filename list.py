878554cc602fbc00979b5095d0aa6b6c7671f57d
M12oooo <88971780+M12oooo@users.noreply.github.com>
2022-03-31 21:54:09 +0530

    initial commit

---
+++ list.py
@@ -0,0 +1,47 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+# f1 = input("Enter Fruit Number= 1:")
+
+# f2 = input("Enter Fruit Number 2:")
+
+# f3 = input("Enter Fruit Number 3:")
+
+# f4 = input("Enter Fruit Number 4:")
+
+# f5 = input("Enter Fruit Number 5:")
+
+# f6 = input("Enter Fruit Number 6:")
+
+# f7 = input("Enter Fruit Number 7:")
+  
+# myFruitList = [f1,f2,f3,f4,f5,f6,f7]
+# print(myFruitList)
+
+# Write a program to accept the marks of 6 students and 
+# display them in a sorted manner.
+
+'''f1 = input("Enter Marks OF subject 1 : ")
+f2 = input("Enter Marks OF subject 2 : ")
+f3 = input("Enter Marks OF subject 3 : ")
+f4 = input("Enter Marks OF subject 4: ")
+f5 = input("Enter Marks OF subject 5: ")
+f6 = input("Enter Marks OF subject 6: ")
+
+mylist = [f1,f2,f3,f4,f5,f6]
+mylist.sort()
+print(mylist)'''
+
+
+# Check that a tuple cannot be changed in Python.
+
+
+
+a = (7, 0, 8, 0, 0, 9)
+
+
+print(a.count(0))
+
+a=[2,4,56,7]
+print (sum(a))
+a=(2,4,5,3,2)
+a[0]=45
