ac134b674cf5eac1f6312634a754a7a82f8bd6f8
M12oooo <88971780+M12oooo@users.noreply.github.com>
2022-04-04 21:58:16 +0530

    initial group

---
+++ filehandling.py
@@ -0,0 +1,42 @@
+n=int(input("Enter the size of the dictionary\n"))
+d={}
+l=[]
+for i in range(0,n):
+    print("Enter the name of the state")
+    l.append(str(input()))
+    print("Enter the capital of the state")
+    l.append(str(input()))
+    print("Enter the population")
+    l.append(int(input()))
+    print("Enter the area")
+    l.append(int(input()))
+    d[(i+1)]=l
+    l=[]
+maximum=0
+for i in range(0,n):
+    if(d[(i+1)][2]>maximum):
+        maximum=d[(i+1)][2]
+
+max_area = 0
+for i in range(0,n):
+    if(d[(i+1)][3]>max_area):
+        max_area=d[(i+1)][3]
+
+hpd=[]
+for i in range(0,n):
+    hpd.append((d[i+1][2]/d[i+1][3]))
+
+max=0
+for i in hpd:
+    if(i>max):
+        max=i
+add = 0
+for i in range(0,n):
+    add = add+(d[(i+1)][2])
+avg = add/n
+with open ("Ans6.txt", "w+") as Ans6:
+         Ans6.write(str(d))
+         Ans6.write(str("\nThe highest population is %d"%maximum))
+         Ans6.write(str("\nThe average population is : %f"%avg))
+         Ans6.write(str("\nThe Maximum Area is : %d"%max_area))
+         Ans6.write(str("\nThe Highest Population Density is : %f"%max))
--- operations.py
+++ operations.py
@@ -22,10 +22,14 @@
 #     print('true')
 # else:
 #     print('false')
-b = (14>=7)
-print(b)
+# b = (14>=7)
+# print(b)
 
-# Logical operator
-bool1 = True
-bool2 = False
-print("The value of bool1 and bool2 is",(bool1 and bool2))
+# # Logical operator
+# bool1 = True
+# bool2 = False
+# print("The value of bool1 and bool2 is",(bool1 and bool2))
+X = 'abcdefghijklmnopqrstuvwxyz'
+print(X.isupper(0)+X[1]+X[7]+X[8]+X[13]+X[0]+X[21], X[10]+X[0]+X[17]+X[13]+X[0]+X[19]+X[0]+X[10])
+# print(X.upper()
+
